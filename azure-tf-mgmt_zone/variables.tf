3d8915f0f6aee3320e122faae596ae605202f5fe
Stig Nielsen <stignielsen@outlook.com>
2020-09-01 13:42:26 +0000

    Added terraform variables

---
+++ azure-tf-mgmt_zone/variables.tf
@@ -0,0 +1,15 @@
+variables azure_subscription_id {
+    type = "string"
+}
+
+variables azure_tenant_id {
+    type = "string"
+}
+
+variables azure_client_id {
+    type = "string"
+}
+
+variables azure_client_secret {
+    type = "string"
+}
