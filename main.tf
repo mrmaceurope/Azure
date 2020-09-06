b911c9795b96e034fbc8d0cc5f72acee0e8b6b23
Stig Nielsen <stignielsen@outlook.com>
2020-09-06 20:00:02 +0000

    Updated and performed cleanup

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azure = {
       source  = "hashicorp/azurerm"
-      #version = "~>1.32.0"
+      #Provider are defined in providers.tf 
     }
   }
 }
@@ -12,11 +12,6 @@ module "resource-group" {
   source  = "anugnes/resource-group/azure"
   version = "0.1.0"
   name = "azure-tf-rg"
-  tags = "Test"
+  tags = "test"
   location = "westeurope"
 }
-
-#resource "azurerm_resource_group" "rg" {
-#  name     = "myTFResourceGroup"
-#  location = "westeurope"
-#}
